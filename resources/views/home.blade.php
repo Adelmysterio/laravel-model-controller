69cad2e4f91e4af3bc97683f014506ec4b55ea45
AdelMysterio <storliniadelmo@gmail.com>
2024-07-17 13:32:34 +0200

    mostrati dati correttamente dal db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
 @section('main-content')
-
-<main>
-prova
-</main>
-
+    <main class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <article class="col-4">
+                    <p>{{ $movie->id }}</p>
+                    <h2>{{ $movie->title }}</h2>
+                    <pre>{{ $movie->original_title }}</pre>
+                    <p>{{ $movie->nationality }}</p>
+                    <p>{{ $movie->date }}</p>
+                    <p>{{ $movie->vote }}</p>
+                </article>
+            @endforeach
+        </div>
+    </main>
 @endsection
