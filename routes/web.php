9b7bd7f17c1045face77be1948790a868663a623
AdelMysterio <storliniadelmo@gmail.com>
2024-07-17 12:54:16 +0200

    fixato errore in routs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'PageController@index');
+Route::get('/', [PageController::class, 'index'])->name('guest.index');
